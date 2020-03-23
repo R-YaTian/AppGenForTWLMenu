45cc1747f489f9287c820c3e7fc06111bccb710b
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2020-03-23 14:16:32 +0800

    V1.3

---
+++ AppGen.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+if os.path.exists("dsiware"):
+    shutil.rmtree("dsiware")
+os.mkdir("dsiware")
+
+for app in os.listdir():
+    try:
+        for titleid in os.listdir(app + "/data/"):
+            if titleid.endswith(".sav"):
+                if titleid.startswith("pri"):
+                    print("/{}/data/{}".format(app, titleid))
+                    shutil.copy("{}/data/{}".format(app, titleid), "dsiware")
+                    os.rename("dsiware/{}".format(titleid), "dsiware/{}.prv".format(app))
+                if titleid.startswith("pub"):
+                    print("/{}/data/{}".format(app, titleid))
+                    shutil.copy("{}/data/{}".format(app, titleid), "dsiware")
+                    os.rename("dsiware/{}".format(titleid), "dsiware/{}.pub".format(app))
+        for title in os.listdir(app + "/content/"):
+            if title.endswith(".app"):
+                print("/{}/content/{}".format(app, title))
+                shutil.copy("{}/content/{}".format(app, title), "dsiware")
+                os.rename("dsiware/{}".format(title), "dsiware/{}.nds".format(app))
+        os.remove("dsiware/484e474a.prv")
+        os.remove("dsiware/484e474a.nds")
+        os.remove("dsiware/534c524e.nds")
+        os.remove("dsiware/53524c41.nds")
+    except:
+        pass
