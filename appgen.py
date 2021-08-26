20771701087f3d5de8fdd8ad605c2f0c290b0cf2
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2021-08-26 17:23:40 +0800

    rewrite

--- appgen.py
+++ appgen.py
@@ -1,63 +1,47 @@
-import os
-import shutil
+from os import path, getcwd, mkdir, listdir
+from shutil import rmtree, copyfile
 
 def agen(apath, dst):
-    print('AppGen V1.7 - BY R-YaTian')
+    print('AppGen V1.8 - BY R-YaTian')
 
-    files = []
+    files = ['dsiware', 'appgen.py', 'Appgen.exe', '53524c41', '484e474a', '534c524e']
 
-    dware = os.path.join(dst, 'dsiware')
-    if os.path.exists(dware):
-        shutil.rmtree(dware)
-    os.mkdir(dware)
+    dware = path.join(dst, 'dsiware')
+
+    if dst == getcwd():
+        if path.exists(dware):
+            rmtree(dware)
+        mkdir(dware)
 
     num = 0
 
-    for app in os.listdir(apath):
-        apps = apath + '\\' + app
+    for app in listdir(apath):
+        if app in files:
+            continue
+        apps = path.join(apath, app)
         try:
-            for title in os.listdir(apps + '\\' + 'content'):
+            for title in listdir(path.join(apps, 'content')):
                 if title.endswith('.app'):
-                    if app != '53524c41' and app != '484e474a' and app != '534c524e' :
-                        print('{}\content\{}'.format(app, title))
-                    shutil.copy(apath + '\{}\content\{}'.format(app, title), dware)
-                    os.rename(dware + '\{}'.format(title), dware + '\{}.nds'.format(app))
+                    print('{}/content/{}'.format(app, title))
+                    copyfile(path.join(apath, app, 'content', title), path.join(dware, app + '.nds'))
                     num += 1
         except:
             pass
 
         try:
-            for titleid in os.listdir(apps + '\\' + 'data'):
+            for titleid in listdir(path.join(apps, 'data')):
                 if titleid.endswith('.sav'):
                     if titleid.startswith('pri'):
-                        if app != '484e474a':
-                            print('{}\data\{}'.format(app, titleid))
-                        shutil.copy(apath + '\{}\data\{}'.format(app, titleid), dware)
-                        os.rename(dware + '\{}'.format(titleid), dware + '\{}.prv'.format(app))
+                        print('{}/data/{}'.format(app, titleid))
+                        copyfile(path.join(apath, app, 'data', titleid), path.join(dware, app + '.prv'))
                     if titleid.startswith('pub'):
-                        print('{}\data\{}'.format(app, titleid))
-                        shutil.copy(apath + '\{}\data\{}'.format(app, titleid), dware)
-                        os.rename(dware + '\{}'.format(titleid), dware + '\{}.pub'.format(app))
+                        print('{}/data/{}'.format(app, titleid))
+                        copyfile(path.join(apath, app, 'data', titleid), path.join(dware, app + '.pub'))
         except:
             pass
 
-    if os.path.exists(dware + '\\' + '484e474a.prv'):
-        files.append(dware + '\\' + '484e474a.prv')
-    if os.path.exists(dware + '\\' + '484e474a.nds'):
-        files.append(dware + '\\' + '484e474a.nds')
-    if os.path.exists(dware + '\\' + '534c524e.nds'):
-        files.append(dware + '\\' + '534c524e.nds')
-    if os.path.exists(dware + '\\' + '53524c41.nds'):
-        files.append(dware + '\\' + '53524c41.nds')
-
-    while len(files) > 0:
-        try:
-            os.remove(files.pop())
-        except:
-            pass
-
-    if num == 0:
-        shutil.rmtree(dware)
+    if num == 0 and dst == getcwd():
+        rmtree(dware)
 
 if __name__ == '__main__':
-    agen(os.getcwd(), os.getcwd())
+    agen(getcwd(), getcwd())
